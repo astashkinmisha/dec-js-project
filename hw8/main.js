1fa8870c585eb1ac24982f804990581a9f89aa70
mishaastashkin <trickerforyou@gmail.com>
2022-03-08 15:59:14 +0200

    hw8

---
+++ hw8/main.js
@@ -0,0 +1,44 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+//     - Напишіть код,  котрий :
+//
+// -- отримує текст з параграфа з id "content"
+
+// let textFromP = document.getElementById('content');
+// console.log( textFromP );
+
+// -- отримує текст з блоку з id "rules"
+//
+// let textFromBlock = document.getElementById('rules');
+// console.log( textFromBlock );
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+//
+// let changeTextFromP = document.getElementById('content');
+// changeTextFromP.innerText = 'Lorem Ipsum'
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+//
+// let changeTextOfP = document.getElementById('rules');
+// changeTextOfP.innerText = 'LoremIpsum';
+
+// -- змініть кожному елементу колір фону на червоний
+//document.body.style.backgroundColor = "#AA0000"
+
+// -- змініть кожному елементу колір тексту на синій
+//
+// document.body.style.color = 'blue';
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+//
+// let getListofClasses = document.getElementById('rules');
+// for (const listofClass of getListofClasses.classList) {
+//     console.log( listofClass );
+// }
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+//
+// let changeElTextColor = document.getElementsByClassName('fc_rules');
+//
+// for (let changeElTextColorElement of changeElTextColor) {
+//     changeElTextColorElement.style.color = 'red';
+// }
